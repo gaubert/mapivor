8b788f8f94e2d04abf923e47c043c492d08136e3
Guillaume Aubert <guillaume.aubert@gmail.com>
2015-11-17 18:12:30 +0100

    move to the server side

--- mapivor-server/app.js
+++ mapivor-server/app.js
@@ -1,6 +1,24 @@
 var http = require('http');
 var express = require('express');
+var fs = require('fs');
+
+
 var app = express();
+
+//enable cross domain request by adding an option request
+var allowCrossDomain = function(req, res, next) {
+    if ('OPTIONS' == req.method) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -35,9 +53,36 @@ app.get('/wms-get-capability', function (req, res) {
 
 });
 
+app.get('/bundle.js', function(req, res) {
+   var data = fs.readFileSync('./bundle.js','utf8');
+   res.send(data);
+});
+
+app.get('/map', function (req, res) {
+  console.log("in map");
+
+  var filePath = "./index.html";
+
+  var data = fs.readFileSync('./index.html','utf8');
+
+  /*fs.readFile(filePath, function (err, data) {
+      if (err) {
+         throw err;
+      }
+      console.log(data);
+      res.send(data);
+  });*/
+
+  console.log(data);
+  res.send(data);
+
+});
+
 // the rest
 app.get('*', function (req, res) {
-  res.send('Error Hello World!');
+  console.log("error " + req);
+  res.send('Error bad request');
+  
 });
 
 var server = app.listen(3000, function () {
