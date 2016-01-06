f9a5c0cd1dff51808c66b10dc557be15270f33e7
Guillaume Aubert <guillaume.aubert@gmail.com>
2016-01-06 22:06:09 +0100

    experiment with css

--- mapivor-server/eumetview-openlayers.js
+++ mapivor-server/eumetview-openlayers.js
@@ -20,12 +20,25 @@ var isDefined = function isDefined(x) {
     return x !== undefined;
 };
 
+function resizeWindow() {
+  $(window).resize(function () {
+      //$('#log').append('<div>Handler for .resize() called.</div>');
+         var canvasheight=$('#map').parent().css('height');
+         var canvaswidth=$('#map').parent().css('width');
+
+         $('#map').css("height", canvasheight);
+         $('#map').css("width", canvaswidth);
+
+   });
+}
 // end of util functions
 
 /*
   Document ready. Install buttons outside the map for the time navigation
  */
 $(document).ready(function() {
+
+
 
     setNavigationButtons(); // install jquery-ui navigation buttons
 
